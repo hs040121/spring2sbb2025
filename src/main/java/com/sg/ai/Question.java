c5f2800d61863b957ea70521e598c5d0bde6d6b1
108-0 <2023143029@g.shingu.ac.kr>
2025-04-28 16:05:54 +0900

    Question, Answer 테이블만들기

---
+++ src/main/java/com/sg/ai/Question.java
@@ -0,0 +1,25 @@
+package com.sg.ai;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Question {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length=200)
+	private String subject;
+	
+	@Column(columnDefinition = "TEXT")	
+	private String content;
+	
+	private LocalDateTime createDate;
+}
